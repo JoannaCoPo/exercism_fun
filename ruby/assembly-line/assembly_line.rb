cc22036cdadd2d7183f9f4daab80ee4ea0c2d96c
Joanna Cortes <jococopo@gmail.com>
2024-01-30 13:09:51 -0700

    Calculate and test working_items_per_minute"

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -22,15 +22,6 @@ class AssemblyLine
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour/60).to_i
   end
 end
-
-# The assembly line's speed can range from 0 (off) to 10 (maximum).
-
-# At its slowest speed (1), 221 cars are produced each hour. The production increases linearly with the speed. So with the speed set to 4, it should produce 4 * 221 = 884 cars per hour. However, higher speeds increase the likelihood that faulty cars are produced, which then have to be discarded. The following table shows how speed influences the success rate:
-
-# 1 to 4: 100% success rate.
-# 5 to 8: 90% success rate.
-# 9: 80% success rate.
-# 10: 77% success rate.
