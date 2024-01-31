8b34978b8925f15c3714d74d6cbab82d90d7eb1f
Joanna Cortes <jococopo@gmail.com>
2024-01-31 09:30:08 -0700

    Card parsing logic

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,6 +1,20 @@
 module Blackjack
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    case card
+    when "ace" then 11
+    when "two" then 2
+    when "three" then 3
+    when "four" then 4
+    when "five" then 5
+    when "six" then 6
+    when "seven" then 7
+    when "eight" then 8
+    when "nine" then 9
+    when "ten", "jack", "queen", "king"
+      10
+    else
+      0
+    end
   end
 
   def self.card_range(card1, card2)
@@ -11,3 +25,17 @@ module Blackjack
     raise "Please implement the Blackjack.first_turn method"
   end
 end
+
+# 1. Calculate the value of any given card.
+# You will receive two cards and will be able to see the face up card of the dealer. 
+# All cards are represented using a string such as "ace", "king", "three", "two", etc. 
+# The values of each card are:
+
+# Commonly, aces can take the value of 1 or 11 but for simplicity we will assume that they can only 
+# take the value of 11.
+
+# Implement the method Blackjack.parse_card which takes a card as a string as an argument. 
+# The method should return the value of the card as an integer.
+
+# Blackjack.parse_card("ace")
+# => 11
