3a36c2c61860b2b6b92e7c23b990076e859989f7
Joanna Cortes <jococopo@gmail.com>
2024-02-01 11:17:10 -0700

    Fix bird log

--- javascript/bird-watcher/bird-watcher.js
+++ javascript/bird-watcher/bird-watcher.js
@@ -8,13 +8,7 @@ export function totalBirdCount(birdsPerDay) {
   }
   return total;
 }
-/**
- * Calculates the total number of birds seen in a specific week.
- *
- * @param {number[]} birdsPerDay
- * @param {number} week
- * @returns {number} birds counted in the given week
- */
+
 export function birdsInWeek(birdsPerDay, week) {
   let total = 0;
   for (let index = 7 * (week - 1); index < (7 * week); index++) {
@@ -23,13 +17,10 @@ export function birdsInWeek(birdsPerDay, week) {
   return total;
 }
 
-/**
- * Fixes the counting mistake by increasing the bird count
- * by one for every second day.
- *
- * @param {number[]} birdsPerDay
- * @returns {number[]} corrected bird count data
- */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Please implement the fixBirdCountLog function');
+  for (let index = 0; index < birdsPerDay.length; index += 2) {
+    birdsPerDay[index] = birdsPerDay[index] += 1;
+  }
+  return birdsPerDay;
 }
+
