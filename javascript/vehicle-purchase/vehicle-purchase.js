f4864bcee29ceda0681d7fd28272e8890025e372
Joanna Cortes <jococopo@gmail.com>
2024-02-01 09:18:46 -0700

    Pass all tests

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -9,14 +9,12 @@ export function chooseVehicle(option1, option2) {
   return `${chosenOne[0]} is clearly the better choice.`;
 }
 
-/**
- * Calculates an estimate for the price of a used vehicle in the dealership
- * based on the original price and the age of the vehicle.
- *
- * @param {number} originalPrice
- * @param {number} age
- * @returns {number} expected resell price in the dealership
- */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3 ) {
+    return originalPrice * 0.8;
+  } else if (age > 10) {
+    return originalPrice * 0.5; 
+  } else {
+    return originalPrice * 0.7;
+  }
 }
