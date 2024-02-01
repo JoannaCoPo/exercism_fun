a39cea4ac88b295728f3b4010f7f959cdf107261
Joanna Cortes <jococopo@gmail.com>
2024-02-01 10:13:13 -0700

    Download and start bird-watcher

---
+++ javascript/bird-watcher/bird-watcher.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+export function totalBirdCount(birdsPerDay) {
+  let total = 0;
+  for (let index = 0; index < birdsPerDay.length; index++) {
+    total += birdsPerDay[index];
+    // accumulates the sum of all the elements in the array
+  }
+  return total;
+}
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  throw new Error('Please implement the birdsInWeek function');
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  throw new Error('Please implement the fixBirdCountLog function');
+}
