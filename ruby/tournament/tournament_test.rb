795f1e4e0b1534181b3203e49fa285a5eacfeb6e
Joanna Cortes <jococopo@gmail.com>
2024-02-08 15:44:53 -0700

    Tally per outcome and start on formatting

--- ruby/tournament/tournament_test.rb
+++ ruby/tournament/tournament_test.rb
@@ -1,9 +1,10 @@
 require 'minitest/autorun'
 require_relative 'tournament'
+require 'pry'
 
 class TournamentTest < Minitest::Test
   def test_just_the_header_if_no_input
-    # skip
+    skip
     input = <<~INPUT
 
     INPUT
@@ -92,7 +93,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_winner
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;loss
       Allegoric Alaskans;Blithering Badgers;win
